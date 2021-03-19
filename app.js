fbcf0a8f76ce942c7c2278262f1c2ab252f7c967
BorisFlores <borisfc23@gmail.com>
2021-03-19 18:49:41 -0500

    com-2

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+                            //user:pwd@servidor:puerto/name_bd
+mongoose.connect('mongodb://dev:dev@localhost:27017/todos',{useNewUrlParser:true,useUnifiedTopology:true});
+
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log('Conexion a la BD');    
+});
+connection.on('error',(err)=>{
+    console.log('Error Conexion a la BD',err);    
+});
+
 app.get('/',(req,res)=>{
     res.json({
-        response:'success'
+        response:'success ,ok'
     })
 });
 app.listen(3000,()=>{
